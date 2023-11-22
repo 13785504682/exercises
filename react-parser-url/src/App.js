7d9beac8d353e6c0f93b48711c7869d7887e157b
xxx <2099284754@qq.com>
2023-11-22 10:45:48 +0800

    做完

--- react-parser-url/src/App.js
+++ react-parser-url/src/App.js
@@ -10,27 +10,81 @@ const defaultParserResult = {
   hash: ''
 }
 
-const parserUrl = (url) => {
-  // 你的实现
-  return defaultParserResult;
-};
+// const parserUrl = (url) => {
+//   // 你的实现
+//   return defaultParserResult;
+// };
 
 // 测试用例
-parserUrl('https://baidu.com:443/s?wd=hello');
+// parserUrl('https://baidu.com:443/s?wd=hello');
 // 输出结果：{ protocol: 'https:', hostname: 'baidu.com', port: '443', pathname: '/s', params: { wd: 'hello' },  hash: '' }
 
 
 function App() {
   const [result, setResult] = useState(defaultParserResult);
+  let [url, setInputUrl] = useState('');
 
-  useEffect(() => {
-    const onKeyDown = (e) => {
-      if (e.keyCode === 13) {
-        console.log('这里 处理 Enter 事件');
-        setResult(defaultParserResult);
+  function changeurl() {
+    //分离协议
+    let pindex = url.indexOf('://')
+    if (pindex !== -1) {
+      defaultParserResult.protocol = url.slice(0, pindex)
+      console.log(pindex)
+      url = url.slice(pindex + 3);
+    }
+
+    //分离哈希
+    let hashindex = url.indexOf('#')
+    if (hashindex !== -1) {
+      defaultParserResult.hash = url.slice(hashindex + 1);
+      url = url.slice(0, hashindex)
+    }
+
+    //分离查询参数部分
+    let queryindex = url.indexOf('?')
+    if (queryindex !== -1) {
+      let queryString = url.slice(queryindex + 1);
+      url = url.slice(0, queryindex)
+      let paramArray = queryString.split('&')
+      for (let param of paramArray) {
+        let [key, value] = param.split('=')
+        defaultParserResult.params[key] = decodeURIComponent(value)
       }
     }
-    document.addEventListener('keydown', onKeyDown, false);
+
+    //分离端口部分
+    let portindex = url.indexOf(':')
+    let pathindex = url.indexOf('/');
+
+    if (portindex !== -1 && (pathindex === -1 || portindex < pathindex)) {
+      defaultParserResult.hostname = url.slice(0, portindex)
+      defaultParserResult.port = url.slice(portindex + 1, pathindex)
+    } else {
+      defaultParserResult.hostname = url.slice(0, pathindex !== -1 ? pathindex : undefined)
+    }
+
+    //获取路径名部分
+    defaultParserResult.pathname = url.slice(pathindex !== -1 ? pathindex : undefined)
+    return defaultParserResult;
+  }
+  const parserUrl = () => {
+    changeurl()
+    console.log(defaultParserResult);
+    setResult(defaultParserResult)
+  }
+
+  const changeinput = (e) => {
+    setInputUrl(e.target.value)
+  }
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      console.log('这里 处理 Enter 事件');
+      changeurl()
+      console.log(defaultParserResult);
+      setResult(defaultParserResult);
+    }
+  }
+  useEffect(() => {
   }, []);
 
   return (
@@ -41,8 +95,11 @@ function App() {
         <div>并将结果渲染在页面上（tips: 请尽可能保证 parserUrl 的健壮性和完备性）</div>
       </header>
       <section className="App-content">
-        <input type="text" placeholder="请输入 url 字符串" />
-        <button id="J-parserBtn">解析</button>
+        <input type="text" placeholder="请输入 url 字符串"
+          onChange={(e) => changeinput(e)}
+          onKeyDown={(e) => onKeyDown(e)}
+        />
+        <button id="J-parserBtn" onClick={() => parserUrl()}>解析</button>
       </section>
       <section className="App-result">
         <h2>解析结果</h2>
--- react-remote-add/src/App.js
+++ react-remote-add/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-
+import { useState } from "react";
 const addRemote = async (a, b) =>
   new Promise((resolve) => {
     setTimeout(() => resolve(a + b), 100);
   });
 
 // 请实现本地的add方法，调用 addRemote，能最优的实现输入数字的加法。
-async function add(...inputs) {
-  // 你的实现
-}
+// async function add(...inputs) {
+//   // 你的实现
+// }
 
 /**
  * 要求：
@@ -18,6 +18,66 @@ async function add(...inputs) {
  * 3. 计算时间越短越好
  */
 function App() {
+  const [inputs, setInputs] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [inputError, setInputError] = useState(false);
+  const [isCalculating, setIsCalculating] = useState(false);
+  let [time,setTime] = useState(0)
+  let [result,setResult] = useState(0)
+
+  const add = async (...inputs) => {
+    let sum = 0;
+    for (let i = 0; i < inputs.length; i += 2) {
+      if (i + 1 >= inputs.length) {
+        sum += inputs[i];
+      } else {
+        const a = inputs[i];
+        const b = inputs[i + 1];
+        sum += await addRemote(a, b);
+      }
+    }
+    return sum;
+  };
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+    setInputError(false);
+  };
+
+  const handleAddClick = async () => {
+    const numbers = inputText
+      .replace(/\s/g, '') 
+      .split(',')
+      .map((num) => Number(num));
+
+    // 验证输入是否合法
+    if (numbers.some((num) => isNaN(num))) {
+      setInputError(true);
+      return;
+    }
+
+    setIsCalculating(true); 
+    setInputs(numbers);
+
+    try {
+      const start = new Date(); 
+       result = await add(...numbers); 
+      const end = new Date(); 
+       time = end - start;
+
+      setTime(time)
+
+      setInputText(''); 
+      setInputs([]); 
+      setIsCalculating(false); 
+      setResult(result)
+    } catch (e) {
+      console.error(e);
+      setIsCalculating(false);
+      alert('计算出错，请重试！');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -31,13 +91,17 @@ function App() {
         </div>
       </header>
       <section className="App-content">
-        <input type="text" placeholder="请输入要相加的数字（如1,4,3,3,5）" />
-        <button>相加</button>
+        <input type="text" placeholder="请输入要相加的数字（如1,4,3,3,5）" value={inputText}
+          onChange={handleInputChange}
+          style={{ borderColor: inputError ? 'red' : '' }} // 根据输入是否合法改变边框颜色
+          disabled={isCalculating} />
+        <button onClick={handleAddClick} disabled={inputError || isCalculating}>
+          {isCalculating ? '计算中...' : '相加'}</button>
       </section>
       <section className="App-result">
         <p>
           相加结果是：
-          {"{你的计算结果}"}， 计算时间是：{"{你的计算时间}"} ms
+          {result}， 计算时间是：{time} ms
         </p>
       </section>
     </div>
